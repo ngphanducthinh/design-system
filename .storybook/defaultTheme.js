d961383891d5fa27fbf8d333f92744722968ba9e
Nguyen, DucThinh <ducthinh.nguyen@rakuten.com>
2024-01-29 13:36:59 +0700

    Show package version on storybook

--- .storybook/defaultTheme.js
+++ .storybook/defaultTheme.js
@@ -1,7 +1,8 @@
 import { create } from '@storybook/theming/create';
+import packageJson from '../package.json';
 
 export default create({
   base: 'dark',
-  brandTitle: `Design System`,
+  brandTitle: `Design System ${packageJson.version}`,
   // brandImage: 'logo.webp',
 });
