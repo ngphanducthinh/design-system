8ccb108bdd66e419e9a8196b307df62dbeb74bce
ducthinh.nguyen <ducthinh.nguyen@rakuten.com>
2024-04-19 16:32:44 +0700

    Increase timeout for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,12 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    actionTimeout: 10 * 1000,
+    navigationTimeout: 8 * 1000,
+  },
+  expect: {
+    timeout: 10 * 1000,
   },
 
   /* Configure projects for major browsers */
