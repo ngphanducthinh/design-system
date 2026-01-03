281f672d911cd012a5ab9f5a47c7baa3c87a2e8b
ngphanducthinh <ngphanducthinh@gmail.com>
2026-01-03 15:44:00 +0700

    init build package

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import './assets/main.css';
+
+export * from './components';
+export * from './types';
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { URL, fileURLToPath } from 'node:url'
-
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +13,22 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  build: {
+    outDir: './dist',
+    // https://vite.dev/guide/build#library-mode
+    lib: {
+      name: 'DesignSystem',
+      fileName: 'design-system',
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    rolldownOptions: {
+      // Vue is provided by the parent project, don't compile Vue source-code inside our library.
+      external: ['vue'],
+      output: {
+        sourcemap: true,
+        preserveModules: true,
+      },
+    },
+  },
+});
